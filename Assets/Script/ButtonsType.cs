746b6e1a83eb03ac131e560a0d8e859e0c877fc0
MarcoAs25 <marcoas2566@gmail.com>
2023-10-22 16:43:38 -0300

    merge

---
+++ Assets/Script/ButtonsType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GMJP/Button", fileName = "New Button")]
+public class ButtonsType : ScriptableObject
+{
+    public string buttonText;
+    public Sprite buttonIcon;
+}
---
+++ Assets/Script/MenuButtons.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuButtons : MonoBehaviour
+{
+
+    [Header("Button Type")]
+    [SerializeField] private ButtonsType ButtonType;
+
+    private TextMeshProUGUI buttonText;
+
+    private Image buttonImage;
+
+    private Color imageOpacity;
+
+    private void Awake()
+    {
+        buttonText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = ButtonType.buttonText;
+
+        buttonImage = gameObject.GetComponent<Image>();
+        imageOpacity = buttonImage.color;
+
+        if (ButtonType.buttonIcon != null)
+        {
+            imageOpacity.a = 1;
+            buttonImage.color = imageOpacity;
+            buttonImage.sprite = ButtonType.buttonIcon;
+        }
+    }
+}
