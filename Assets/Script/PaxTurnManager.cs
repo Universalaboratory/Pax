e06f809159b3a70fec5bd0d6728d63ea915768b0
Lucas Faria <50430652+FariaLukas@users.noreply.github.com>
2023-10-22 22:40:33 -0300

    Sounds

--- Assets/Script/PaxTurnManager.cs
+++ Assets/Script/PaxTurnManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Photon;
+using TMPro;
 
 public class PaxTurnManager : PunBehaviour
 {
@@ -7,6 +8,8 @@ public class PaxTurnManager : PunBehaviour
     [SerializeField] private bool _isMasterTurn = true;
     [SerializeField] private bool _myTurn = false;
     [SerializeField] private float timeTurn = 20f;
+    [SerializeField] private TextMeshProUGUI TurnDisplay;
+    private CardHand _hand;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -22,24 +25,49 @@ public class PaxTurnManager : PunBehaviour
         {
             _myTurn = true;
         }
+        UpdateUI();
     }
+
     public bool isMyTurn()
     {
         return _myTurn;
+    }
+    
+    private void UpdateUI()
+    {
+        if (_myTurn)
+        {
+            TurnDisplay.text = $"{PhotonNetwork.player.NickName} Turn";
+        }
+        else
+        {
+            foreach (var player in PhotonNetwork.playerList)
+            {
+                if (player != PhotonNetwork.player)
+                {
+                    TurnDisplay.text = $"{player.NickName} Turn";
+                }
+            }
+        }
     }
 
     public void ToggleTurn()
     {
         _isMasterTurn = !_isMasterTurn;
         _myTurn = false;
-        if(PhotonNetwork.isMasterClient && _isMasterTurn)
+
+        if (PhotonNetwork.isMasterClient && _isMasterTurn)
         {
             _myTurn = true;
+            TurnDisplay.text = $"{PhotonNetwork.player.NickName} Turn";
         }
-        if(!PhotonNetwork.isMasterClient && !_isMasterTurn)
+        if (!PhotonNetwork.isMasterClient && !_isMasterTurn)
         {
             _myTurn = true;
+
+
         }
+        UpdateUI();
         WinManager.Instance.hasChampion();
     }
 }
