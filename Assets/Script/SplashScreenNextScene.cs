93959090142bdb277e9a9c1c37b19d3503c06c1e
OGabrielfo <ogabrielfo.silva@gmail.com>
2024-01-07 20:19:05 -0300

    SplashScreen
    Animação da SplashScreen feita.
    
    Necessário adicionar logotipo da equipe e também algum som de fundo.
    
    Não esquecer de adicionar a SplashScreen na sequência de cenas da build.

---
+++ Assets/Script/SplashScreenNextScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(NextScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(10.06f);
+
+        SceneManager.LoadScene("Menu");
+    }
+}
