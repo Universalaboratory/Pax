244fdbe317890fc7cae20dab1cbe44e4bb10e257
OGabrielfo <ogabrielfo.silva@gmail.com>
2024-01-08 12:10:32 -0300

    SplashScreen e Hub
    - Adicionado logo da equipe na splash screen
    - Adicionado função para pular splash screen após 3 segundos.
    - Corrigidos os gráfico da sala no hub da Main.
    - Corrigidos os gráficos do menu de configuração.
    
    - Encontrada falha no menu de configurações da Scene "Menu", a tela de configuração não se encontra nesta cena.

--- Assets/Script/SplashScreenNextScene.cs
+++ Assets/Script/SplashScreenNextScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreenNextScene : MonoBehaviour
 {
+    private float _timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +15,23 @@ public class SplashScreenNextScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _timer += Time.deltaTime;
+
+        if (_timer > 3f  && Input.anyKeyDown)
+        {
+            GoToMenu();
+        }
     }
 
     IEnumerator NextScene()
     {
         yield return new WaitForSeconds(10.06f);
 
+        GoToMenu();
+    }
+
+    private void GoToMenu()
+    {
         SceneManager.LoadScene("Menu");
     }
 }
